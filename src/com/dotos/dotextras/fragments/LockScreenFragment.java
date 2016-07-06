45394f38eba8d9808b5315b525548f7677e1c06c
beanstown106 <nbenis106@gmail.com>
2016-07-06 16:53:56 +0200

    Fingerprint authentication vibration [2/2]
    Change-Id: I6ebdca09b45549d44819a0742c84404db76f1d23

--- src/com/dotos/dotextras/fragments/LockScreenFragment.java
+++ src/com/dotos/dotextras/fragments/LockScreenFragment.java
@@ -1,19 +1,34 @@
 package com.dotos.dotextras.fragments;
 
 import android.os.Bundle;
+import android.content.Context;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.hardware.fingerprint.FingerprintManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
 
 import com.dotos.R;
 
 
 public class LockScreenFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+	private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.dot_lockscreen);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        // Fingerprint vibration
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+            mFingerprintVib = (SwitchPreference) prefSet.findPreference("fingerprint_success_vib");
+            if (!mFingerprintManager.isHardwareDetected()){
+                prefSet.removePreference(mFingerprintVib);
+            }
 
     }
 
