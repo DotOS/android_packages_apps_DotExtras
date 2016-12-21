65e9f38f74b9baa3ea833bcd85f5a83eee4cce7d
Pranav Vashi <neobuddy89@gmail.com>
2016-12-21 14:38:52 +0100

    Immersive Recents [2/2]
    Change-Id: I952ce01c0e1b3f5df5414919ccd19c1cd02c4714
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/dotos/dotextras/fragments/RecentsFragment.java
+++ src/com/dotos/dotextras/fragments/RecentsFragment.java
@@ -28,20 +28,22 @@ public class RecentsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         getActivity().getFragmentManager().beginTransaction()
-                .replace(R.id.pref_view_layout, new SettingsPreferenceFragment())
+                .replace(R.id.pref_view_layout, new RecentsFragment())
                 .commit();
     }
 
-    public static class SettingsPreferenceFragment extends PreferenceFragment
+    public static class RecentsFragment extends PreferenceFragment
             implements OnPreferenceChangeListener {
 
-        public SettingsPreferenceFragment() {
+        public RecentsFragment() {
         }
 
         private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+        private static final String IMMERSIVE_RECENTS = "immersive_recents";
 
         private SwitchPreference mRecentsClearAll;
         private ListPreference mRecentsClearAllLocation;
+        private ListPreference mImmersiveRecents;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -61,17 +63,30 @@ public class RecentsFragment extends PreferenceFragment {
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
             mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
+	    // Immersive recents
+            mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
+            mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
+                    resolver, Settings.System.IMMERSIVE_RECENTS, 0)));
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            mImmersiveRecents.setOnPreferenceChangeListener(this);
+
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             ContentResolver resolver = getActivity().getContentResolver();
             if (preference == mRecentsClearAllLocation) {
-                int location = Integer.valueOf((String) newValue);
+                int location = Integer.parseInt((String) newValue);
                 int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
                 Settings.System.putIntForUser(getActivity().getContentResolver(),
                         Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
                 mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+                return true;
+            } else if (preference == mImmersiveRecents) {
+                Settings.System.putInt(resolver, Settings.System.IMMERSIVE_RECENTS,
+                        Integer.parseInt((String) newValue));
+                mImmersiveRecents.setValue(String.valueOf(newValue));
+                mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
                 return true;
             }
             return false;
