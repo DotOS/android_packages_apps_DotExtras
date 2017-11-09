b110e80babf2977f09a447277d3b6cbc502a9af6
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-11-09 22:41:48 +0530

    added camera sound toggle
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- src/com/dotos/dotextras/fragments/MiscFragment.java
+++ src/com/dotos/dotextras/fragments/MiscFragment.java
@@ -3,11 +3,19 @@ package com.dotos.dotextras.fragments;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.os.SystemProperties;
 
 import com.dotos.R;
 
 
 public class MiscFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+	private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
+    private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
+
+    private SwitchPreference mCameraSounds;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,10 +23,25 @@ public class MiscFragment extends PreferenceFragment implements Preference.OnPre
 
         addPreferencesFromResource(R.xml.dot_misc);
 
+        mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
+        mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
+        mCameraSounds.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    	final String key = preference.getKey();
+    	if (preference == mCameraSounds) {
+               if (KEY_CAMERA_SOUNDS.equals(key)) {
+                   if ((Boolean) newValue) {
+                       SystemProperties.set(PROP_CAMERA_SOUND, "1");
+                   } else {
+                       SystemProperties.set(PROP_CAMERA_SOUND, "0");
+                   }
+                }
+                return true;
+          }
         return false;
     }
 }
