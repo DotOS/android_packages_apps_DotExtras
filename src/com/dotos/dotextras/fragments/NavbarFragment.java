d248f58fd1bac458ca767d3e79e7aab51d341c43
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-11-12 20:02:52 +0530

    added trigger for CM buttons inside DotExtras
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- src/com/dotos/dotextras/fragments/NavbarFragment.java
+++ src/com/dotos/dotextras/fragments/NavbarFragment.java
@@ -3,6 +3,10 @@ package com.dotos.dotextras.fragments;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.content.ComponentName;
+import android.content.Intent;
 
 import com.dotos.R;
 
@@ -14,6 +18,27 @@ public class NavbarFragment extends PreferenceFragment implements Preference.OnP
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.dot_navbar);
+
+        Preference navpreference = getPreferenceManager().findPreference(getString(R.string.button_pref_title));
+        navpreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+        	public boolean onPreferenceClick(Preference navpreference) {
+		
+		final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		final ComponentName cn = new ComponentName("org.cyanogenmod.cmparts", "org.cyanogenmod.cmparts.input.ButtonSettings");
+
+		intent.setComponent(cn);
+
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		startActivity( intent);
+
+		return true;
+        	}
+        });
 
     }
 
