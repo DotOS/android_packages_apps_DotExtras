7103ac57f1828dc98511cde695a3fb82dcf7a24c
Mazda-- <alex@dirtyunicorns.com>
2018-02-04 00:27:01 +0530

    DotExtras BringUp based on https://github.com/DirtyUnicorns/android_packages_apps_DU-Tweaks Co-authored-by: IacobIonut01 <paulionut2003@gmail.com>

---
+++ src/com/dot/dotextras/DotExtrasFragment.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dot.dotextras;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.dot.dotextras.fragments.System;
+import com.dot.dotextras.views.PagerSlidingTabStrip;
+import com.dot.dotextras.fragments.Lockscreen;
+import com.dot.dotextras.fragments.PowerMenu;
+import com.dot.dotextras.fragments.QuickSettings;
+import com.dot.dotextras.fragments.Recents;
+import com.dot.dotextras.fragments.Statusbar;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.dashboard.SummaryLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DotExtrasFragment extends SettingsPreferenceFragment {
+
+    ViewPager mViewPager;
+    String titleString[];
+    ViewGroup mContainer;
+    PagerSlidingTabStrip mTabs;
+
+    static Bundle mSavedState;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContainer = container;
+        View view = inflater.inflate(R.layout.dotextras, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.pager);
+        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+        FragmentsAdapter fragmentsAdapter = new FragmentsAdapter(getFragmentManager());
+        mViewPager.setAdapter(fragmentsAdapter);
+        mTabs.setViewPager(mViewPager);
+
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DOTEXTRAS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                return false;
+        }
+    }
+
+    class FragmentsAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public FragmentsAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new Statusbar();
+            frags[1] = new QuickSettings();
+            frags[2] = new Recents();
+            frags[3] = new PowerMenu();
+            frags[4] = new Lockscreen();
+			frags[5] = new System();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                    getString(R.string.statusbar),
+                    getString(R.string.quick_settings),
+                    getString(R.string.recents),
+                    getString(R.string.power_menu),
+                    getString(R.string.lockscreen),
+                    getString(R.string.system)};
+        return titleString;
+    }
+    
+        private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                mSummaryLoader.setSummary(this, mContext.getString(R.string.build_tweaks_summary_title));
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
+
+}
+
