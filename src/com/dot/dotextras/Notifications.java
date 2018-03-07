334b41c0f29f0663433d0c476ae71c9681f1417e
SpiritCroc <spiritcroc@gmail.com>
2018-03-07 16:22:38 +0000

    Return of the ticker scroll [2/2]
    Change-Id: Ifb52c59989609cc63ac750a98292acbf9dd5db37
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- src/com/dot/dotextras/Notifications.java
+++ src/com/dot/dotextras/Notifications.java
@@ -49,6 +49,7 @@ import com.android.settings.Utils;
 public class Notifications extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private ListPreference mTickerMode;
+    private ListPreference mTickerAnimation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +67,15 @@ public class Notifications extends SettingsPreferenceFragment implements OnPrefe
                 1, UserHandle.USER_CURRENT);
         mTickerMode.setValue(String.valueOf(tickerMode));
         mTickerMode.setSummary(mTickerMode.getEntry());
+
+        mTickerAnimation = (ListPreference) findPreference("status_bar_ticker_animation_mode");
+        mTickerAnimation.setOnPreferenceChangeListener(this);
+        int tickerAnimationMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ANIMATION_MODE,
+                1, UserHandle.USER_CURRENT);
+        mTickerAnimation.setValue(String.valueOf(tickerAnimationMode));
+        mTickerAnimation.setSummary(mTickerAnimation.getEntry());
+
     }
 
     @Override
@@ -88,6 +98,14 @@ public class Notifications extends SettingsPreferenceFragment implements OnPrefe
             int index = mTickerMode.findIndexOfValue((String) newValue);
             mTickerMode.setSummary(
                     mTickerMode.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mTickerAnimation)) {
+            int tickerAnimationMode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_TICKER_ANIMATION_MODE, tickerAnimationMode, UserHandle.USER_CURRENT);
+            int index = mTickerAnimation.findIndexOfValue((String) newValue);
+            mTickerAnimation.setSummary(
+                    mTickerAnimation.getEntries()[index]);
             return true;
         }
         return false;
