b08769ef82bf2082df4812f52bd24ffd2272671f
IacobIonut01 <paulionut2003@gmail.com>
2018-03-15 23:55:47 +0200

    DotExtras v2.0
    Change-Id: Ie5225ad8e7ae0df0f26883018c55a3852b3b5cd0

--- src/com/dot/dotextras/DotExtrasFragment.java
+++ src/com/dot/dotextras/DotExtrasFragment.java
@@ -46,14 +46,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.dot.dotextras.fragments.System;
-import com.dot.dotextras.views.PagerSlidingTabStrip;
 import com.dot.dotextras.fragments.Lockscreen;
 import com.dot.dotextras.fragments.QuickSettings;
 import com.dot.dotextras.fragments.Recents;
 import com.dot.dotextras.fragments.Statusbar;
 import com.dot.dotextras.fragments.Navigation;
+import com.dot.dotextras.views.HeadCards;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
@@ -64,22 +66,14 @@ import java.util.List;
 
 public class DotExtrasFragment extends SettingsPreferenceFragment {
 
-    ViewPager mViewPager;
-    String titleString[];
     ViewGroup mContainer;
-    PagerSlidingTabStrip mTabs;
 
     static Bundle mSavedState;
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mContainer = container;
         View view = inflater.inflate(R.layout.dotextras, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.pager);
-        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-        FragmentsAdapter fragmentsAdapter = new FragmentsAdapter(getFragmentManager());
-        mViewPager.setAdapter(fragmentsAdapter);
-        mTabs.setViewPager(mViewPager);
-
+		headCardsListener(view);
+		goBackLayout(view);
         setHasOptionsMenu(true);
         return view;
     }
@@ -115,50 +109,86 @@ public class DotExtrasFragment extends SettingsPreferenceFragment {
                 return false;
         }
     }
-
-    class FragmentsAdapter extends FragmentPagerAdapter {
-        String titles[] = getTitles();
-        private Fragment frags[] = new Fragment[titles.length];
-
-        public FragmentsAdapter(FragmentManager fm) {
-            super(fm);
-            frags[0] = new Statusbar();
-            frags[1] = new Navigation();
-            frags[2] = new QuickSettings();
-            frags[3] = new Recents();
-            frags[4] = new Lockscreen();
-	    frags[5] = new System();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles[position];
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return frags[position];
-        }
-
-        @Override
-        public int getCount() {
-            return frags.length;
-        }
+	
+	public void goBackLayout(final View view) {
+        RelativeLayout go_back_layout = view.findViewById(R.id.go_back);
+        go_back_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout frag_layout = view.findViewById(R.id.fragment_head_container);
+                frag_layout.setVisibility(View.GONE);
+                LinearLayout head_layout = view.findViewById(R.id.features_select);
+                head_layout.setVisibility(View.VISIBLE);
+            }
+        });
     }
-
-    private String[] getTitles() {
-        String titleString[];
-        titleString = new String[]{
-                    getString(R.string.statusbar),
-                    getString(R.string.navigation),
-                    getString(R.string.quick_settings),
-                    getString(R.string.recents),
-                    getString(R.string.lockscreen),
-                    getString(R.string.system)};
-        return titleString;
+	
+	public void headCardsListener(final View view) {
+		Fragment frags[] = new Fragment[] {
+            new Statusbar(),
+            new Navigation(),
+            new QuickSettings(),
+            new Recents(),
+            new Lockscreen(),
+			new System()
+        };
+        HeadCards statusbar = view.findViewById(R.id.statusbar_card);
+        statusbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[0]);
+            }
+        });
+        HeadCards navbar = view.findViewById(R.id.navigation_bar_card);
+        navbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[1]);
+            }
+        });
+        HeadCards qs = view.findViewById(R.id.quick_settings_card);
+        qs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[2]);
+            }
+        });
+        HeadCards recents = view.findViewById(R.id.recents_card);
+        recents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[3]);
+            }
+        });
+        HeadCards lockscreen = view.findViewById(R.id.lockscreen_card);
+        lockscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[4]);
+            }
+        });
+        HeadCards system = view.findViewById(R.id.system_card);
+        system.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment(view, frags[5]);
+            }
+        });
+    }
+	
+	public void showFragment(View view, Fragment frag) {
+        LinearLayout frag_layout = view.findViewById(R.id.fragment_head_container);
+        if (frag_layout.getVisibility() != View.VISIBLE) {
+            frag_layout.setVisibility(View.VISIBLE);
+        }
+        LinearLayout head_layout = view.findViewById(R.id.features_select);
+		head_layout.setVisibility(View.GONE);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, frag)
+                .commitAllowingStateLoss();
     }
     
-        private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
 
         private final Context mContext;
         private final SummaryLoader mSummaryLoader;
