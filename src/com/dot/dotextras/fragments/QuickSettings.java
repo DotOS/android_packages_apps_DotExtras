64a1e1b6c06d7e1212ea90a93557b11cd2552b85
Nico60 <ndhouailly@gmail.com>
2018-03-18 16:59:16 +0100

    Quick settings panel : Set transparency [2/2]
    Change-Id: I34a0ca06e99ca050040be1f2a9e9ba7689ea7818

--- src/com/dot/dotextras/fragments/QuickSettings.java
+++ src/com/dot/dotextras/fragments/QuickSettings.java
@@ -32,12 +32,17 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+import com.dot.dotextras.preference.CustomSeekBarPreference;
 import com.android.settings.Utils;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
         private static final String TAG = "QuickSettings";
+
+        private static final String OMNI_QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.quick_settings);
 
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(OMNI_QS_PANEL_BG_ALPHA);
+        int qsPanelAlpha = Settings.System.getIntForUser(resolver,
+                Settings.System.OMNI_QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        mQsPanelAlpha.setValue(qsPanelAlpha);
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -59,13 +70,15 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.OMNI_QS_PANEL_BG_ALPHA, bgAlpha,
+                    UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
     }
 
 }
