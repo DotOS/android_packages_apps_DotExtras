c57ea6a7bc3cb68c58ca4079113e18a5e4b5ac05
Nico60 <ndhouailly@gmail.com>
2018-03-18 16:59:16 +0100

    Quick settings panel : Set transparency [2/2]
    Change-Id: I93ede5c266925a901c53353d2125640088a9637f

--- src/com/dot/dotextras/QuickSettings.java
+++ src/com/dot/dotextras/QuickSettings.java
@@ -36,7 +36,7 @@ import android.view.WindowManagerGlobal;
 import android.view.IWindowManager;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import com.dot.dotextras.preference.CustomSeekBarPreference;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
@@ -50,9 +50,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
     private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
+    private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
 
     private ListPreference mQuickPulldown;
     ListPreference mSmartPulldown;
+    private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,6 +79,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
         mSmartPulldown.setValue(String.valueOf(smartPulldown));
         updateSmartPulldownSummary(smartPulldown);
 
+        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        mQsPanelAlpha.setValue(qsPanelAlpha);
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -135,6 +142,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
             int smartPulldown = Integer.valueOf((String) newValue);
             Settings.System.putInt(resolver, Settings.System.QS_SMART_PULLDOWN, smartPulldown);
             updateSmartPulldownSummary(smartPulldown);
+            return true;
+        } else if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.QS_PANEL_BG_ALPHA, bgAlpha,
+                    UserHandle.USER_CURRENT);
             return true;
         }
         return false;
