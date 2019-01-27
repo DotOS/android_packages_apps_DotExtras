ff6b1af5ae034885c27c20982b5c241c54c26bd3
Mohan Cm <mohanmanjappa@gmail.com>
2019-01-27 22:20:27 +0530

    fix quicksettings fragment
    Change-Id: Ib6a7ad68ceca5c720cd4a93a727fa4aef7ff6c73

--- src/com/dot/dotextras/fragments/QuickSettings.java
+++ src/com/dot/dotextras/fragments/QuickSettings.java
@@ -18,25 +18,36 @@ package com.dot.dotextras.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.dot.dotextras.preference.CustomSeekBarPreference;
 import com.android.settings.Utils;
 
-public class QuickSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+public class QuickSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
         private static final String TAG = "QuickSettings";
 
@@ -50,7 +61,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.quick_settings);
 
-        ContentResolver resolver = getActivity().getContentResolver();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
 
         mQsPanelAlpha = (CustomSeekBarPreference) findPreference(OMNI_QS_PANEL_BG_ALPHA);
         int qsPanelAlpha = Settings.System.getIntForUser(resolver,
@@ -61,7 +73,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.DOTEXTRAS;
+        return MetricsEvent.DOTEXTRAS;
     }
 
     @Override
