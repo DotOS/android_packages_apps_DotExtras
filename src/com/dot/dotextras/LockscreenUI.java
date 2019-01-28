AospExtended/platform_packages_apps_Extensions

Hide fingerprint settings on unsupported devices
Change-Id: I7d11d39123e049a0d9d5e5e194f03fc1da1d0542
Signed-off-by: arghyac35 <arghyac35@gmail.com>

src/org/aospextended/extensions/LockscreenUI.java
@@ -46,7 +46,19 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
+import android.hardware.fingerprint.FingerprintManager;
+import org.aospextended.extensions.preference.SystemSettingSwitchPreference;
+
 public class LockscreenUI extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String FP_CAT = "lockscreen_ui_general_category";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+
+
+    private SystemSettingSwitchPreference mFingerprintVib;
+    private FingerprintManager mFingerprintManager;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,8 +66,26 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
 
         addPreferencesFromResource(R.xml.lockscreen_ui);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
+        ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mResolver = getActivity().getContentResolver();
+        PreferenceScreen prefs = getPreferenceScreen();
+        Resources resources = getResources();
+
+        PreferenceCategory fingerprintCategory = (PreferenceCategory) findPreference(FP_CAT);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+
+         if (!mFingerprintManager.isHardwareDetected()){
+            fingerprintCategory.removePreference(mFingerprintVib);
+        } else {
+        mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+        mFingerprintVib.setOnPreferenceChangeListener(this);
+
+        }
 
     }
 
@@ -71,6 +101,15 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+         if (preference == mFingerprintVib) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
+        
         return false;
     }
 }

DotOS/android_packages_apps_DotExtras

src/com/dot/dotextras/LockscreenUI.java
@@ -47,14 +47,22 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.dot.dotextras.Utils;
 
+import android.hardware.fingerprint.FingerprintManager;
+import com.dot.dotextras.preference.SystemSettingSwitchPreference;
+
 public class LockscreenUI extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_FACE_AUTO_UNLOCK = "face_auto_unlock";
     private static final String KEY_FACE_UNLOCK_PACKAGE = "com.android.facelock";
+    private static final String FP_CAT = "lockscreen_ui_general_category";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 	private static final String LOCK_DATE_FONTS = "lock_date_fonts";
 
     private SwitchPreference mFaceUnlock;
+    private SystemSettingSwitchPreference mFingerprintVib;
+    private FingerprintManager mFingerprintManager;
 	
     ListPreference mLockClockFonts;
 	ListPreference mLockDateFonts;
@@ -75,6 +83,19 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
             mFaceUnlock.setChecked((Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.FACE_AUTO_UNLOCK, 0) == 1));
             mFaceUnlock.setOnPreferenceChangeListener(this);
+        }
+
+        PreferenceCategory fingerprintCategory = (PreferenceCategory) findPreference(FP_CAT);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SystemSettingSwitchPreference) findPreference(FINGERPRINT_VIB);
+
+        if (mFingerprintManager != null && mFingerprintManager.isHardwareDetected()){
+        mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+        mFingerprintVib.setOnPreferenceChangeListener(this);
+        } else {
+        fingerprintCategory.removePreference(mFingerprintVib);
         }
 
         // Lockscren Clock Fonts
@@ -104,6 +125,7 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mFaceUnlock) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getActivity().getContentResolver(),
@@ -120,6 +142,11 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
                     Integer.valueOf((String) objValue));
             mLockDateFonts.setValue(String.valueOf(objValue));
             mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+            return true;
+        } else if (preference == mFingerprintVib) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
             return true;
         }
         return false;