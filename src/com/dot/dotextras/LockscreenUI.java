AospExtended/platform_packages_apps_Extensions

Remove weather preference if package is some how removed/uninstalled
Some users tend to want to 'debloat' ROMs and sometimes are not the
sharpest tool in the shed and remove things that they're not suppose to.

So in the event that a user gets the bright idea of removing the weather
apk, we'll remove the preference. This way we're protected from users that
want to report bogus bugs (because they removed the apk).

Change-Id: I47987ec7793236a702fc5328e668fe65eb7484ee

src/org/aospextended/extensions/fragments/LockscreenUI.java
@@ -50,7 +50,7 @@ import org.aospextended.extensions.Utils;
 
 import android.hardware.fingerprint.FingerprintManager;
 import org.aospextended.extensions.preference.SystemSettingSwitchPreference;
-import com.android.internal.util.custom.weather.WeatherClient;
+import com.android.internal.util.aospextended.AEXUtils;
 
 public class LockscreenUI extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -78,7 +78,7 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
         final PreferenceCategory weatherCategory = (PreferenceCategory) prefSet
                 .findPreference(WEATHER_LS_CAT);
 
-        if (!WeatherClient.isAvailable(getContext())) {
+        if (!AEXUtils.isPackageInstalled(getActivity(), "org.pixelexperience.weather.client")) {
             prefSet.removePreference(weatherCategory);
         }
 

DotOS/android_packages_apps_DotExtras

src/com/dot/dotextras/LockscreenUI.java
@@ -49,7 +49,7 @@ import com.dot.dotextras.Utils;
 
 import android.hardware.fingerprint.FingerprintManager;
 import com.dot.dotextras.preference.SystemSettingSwitchPreference;
-import com.android.internal.util.custom.weather.WeatherClient;
+import com.android.internal.util.dotos.DOTUtils;
 
 public class LockscreenUI extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -83,7 +83,7 @@ public class LockscreenUI extends SettingsPreferenceFragment implements OnPrefer
         final PreferenceCategory weatherCategory = (PreferenceCategory) prefSet
                 .findPreference(WEATHER_LS_CAT);
 
-        if (!WeatherClient.isAvailable(getContext())) {
+        if (!DOTUtils.isPackageInstalled(getActivity(), "org.pixelexperience.weather.client")) {
             prefSet.removePreference(weatherCategory);
         }
 